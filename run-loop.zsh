377929752ab3597ef06ddbf1c44c71be00f5412b
djeanner <djeanner@users.noreply.github.com>
2025-04-23 12:31:38 +0200

    update auto run for auto commit one in a while

--- run-loop.zsh
+++ run-loop.zsh
@@ -39,5 +39,77 @@ while true; do
   echo "Waiting for a while ..."
   sleep 111  # 600 seconds = 10 minutes
 
+
+  # Run your Node script
+  echo "get rts.ch/info page"
+  wget rts.ch/info
+  mv info input.html
+  echo "extract card titles from page"
+  node extract.js
+  echo "extractDayliRemoval into folder removed-by-day"
+  node extractDayliRemoval.js
+  echo "Waiting for a while ..."
+  sleep 113  # 600 seconds = 10 minutes
+
+ # Run your Node script
+  echo "get rts.ch/info page"
+  wget rts.ch/info
+  mv info input.html
+  echo "extract card titles from page"
+  node extract.js
+  echo "extractDayliRemoval into folder removed-by-day"
+  node extractDayliRemoval.js
+  echo "Waiting for a while ..."
+  sleep 107  # 600 seconds = 10 minutes
+
+
+ # Run your Node script
+  echo "get rts.ch/info page"
+  wget rts.ch/info
+  mv info input.html
+  echo "extract card titles from page"
+  node extract.js
+  echo "extractDayliRemoval into folder removed-by-day"
+  node extractDayliRemoval.js
+  echo "Waiting for a while ..."
+  sleep 111  # 600 seconds = 10 minutes
+
+
+  # Run your Node script
+  echo "get rts.ch/info page"
+  wget rts.ch/info
+  mv info input.html
+  echo "extract card titles from page"
+  node extract.js
+  echo "extractDayliRemoval into folder removed-by-day"
+  node extractDayliRemoval.js
+  echo "Waiting for a while ..."
+  sleep 113  # 600 seconds = 10 minutes
+
+ # Run your Node script
+  echo "get rts.ch/info page"
+  wget rts.ch/info
+  mv info input.html
+  echo "extract card titles from page"
+  node extract.js
+  echo "extractDayliRemoval into folder removed-by-day"
+  node extractDayliRemoval.js
+  echo "Waiting for a while ..."
+  sleep 107  # 600 seconds = 10 minutes
+
+
+ # Run your Node script
+  echo "get rts.ch/info page"
+  wget rts.ch/info
+  mv info input.html
+  echo "extract card titles from page"
+  node extract.js
+  echo "extractDayliRemoval into folder removed-by-day"
+  node extractDayliRemoval.js
+  echo "Waiting for a while ..."
+  sleep 111  # 600 seconds = 10 minutes
+
+  git commit html card-title.json -m "update html"
+  git push
 done
 
