4c38a51891f82009c2cd8eba4ee2dc6b029676eb
Damien Jeannerat <djeanner@users.noreply.github.com>
2025-05-02 10:42:17 +0200

    add script or altetnative run

---
+++ run_alternative.zsh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+
+echo "⏳ Starting loop... (Ctrl+C to stop)"
+
+# Define an array of sleep times
+sleep_times=(113 107 111 113 107 111 113 107 111)
+
+while true; do
+  echo "🔁 Running extract-card-titles.js at $(date)"
+
+  for sleep_time in $sleep_times; do
+    echo "Fetching letemps.ch..."
+    wget https://www.letemps.ch -O leTemps.html
+    node extractLeTemps.js
+
+    echo "Fetching rts.ch/info..."
+    wget rts.ch/info -O input.html
+
+    echo "Extracting card titles..."
+    node extract.js
+
+    echo "Waiting for $sleep_time seconds..."
+    sleep $sleep_time
+  done
+
+  # echo "📤 NOT Committing updates to Git..."
+  echo "📤 Committing updates to Git..."
+  git commit html data/ -m "update only main json file"
+  git push
+done
